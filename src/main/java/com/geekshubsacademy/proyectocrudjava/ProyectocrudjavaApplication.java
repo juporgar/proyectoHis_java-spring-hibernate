5d5a80a6ec139fb73f351a19e24270ad4747ccf6
unknown <julian.porgar@gmail.com>
2018-11-10 16:24:35 +0100

    first commit

---
+++ src/main/java/com/geekshubsacademy/proyectocrudjava/ProyectocrudjavaApplication.java
@@ -0,0 +1,12 @@
+package com.geekshubsacademy.proyectocrudjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectocrudjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectocrudjavaApplication.class, args);
+    }
+}
