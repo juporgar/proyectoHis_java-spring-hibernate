c53d010f3ba5f7dadada7e1f83cd152e3b2c2603
unknown <julian.porgar@gmail.com>
2018-11-14 13:34:22 +0100

    Show by console the people in the database

--- src/main/java/com/geekshubsacademy/proyectocrudjava/app/controllers/WebController.java
+++ src/main/java/com/geekshubsacademy/proyectocrudjava/app/controllers/WebController.java
@@ -1,13 +1,23 @@
 package com.geekshubsacademy.proyectocrudjava.app.controllers;
 
+import com.geekshubsacademy.proyectocrudjava.domain.services.IPacientesService;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
+    private static final Log logger = LogFactory.getLog("WebController.class");
+
+    @Autowired
+    private IPacientesService pacientesService;
+
     @GetMapping("/")
     public String inicio(){
+        logger.info(pacientesService.findAll());
         return "index";
     }
 }
